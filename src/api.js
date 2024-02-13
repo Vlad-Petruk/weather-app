e741ae6b2a93e137d33cfe0dbec5e0069cda5faf
Vlad-Petruk <vladpetruk00@gmail.com>
2024-02-13 20:04:39 +0200

    with api.js

---
+++ src/api.js
@@ -0,0 +1,40 @@
+async function getCurrentWeatherData(location) {
+    try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=7a84c19e7f6245b7814141628240402&q=${location}&days=3&aqi=no&alerts=no`,
+        { mode: "cors" },
+      );
+      const weatherData = await response.json();
+      // const weatherDatas = weatherData.current.temp_c;
+      //  console.log(weatherDatas)
+      // console.log(weatherData)
+      return weatherData;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  function processWeatherData(weatherData) {
+    const myWeatherData = {
+      conditions: {
+        icon: weatherData.current.condition.icon,
+        text: weatherData.current.condition.text,
+      },
+      temp_c: weatherData.current.temp_c,
+      feelslike_c: weatherData.current.feelslike_c,
+      temp_f: weatherData.current.temp_f,
+      feelslike_f: weatherData.current.feelslike_f,
+      wind_speed: weatherData.current.wind_kph,
+      cloud: weatherData.current.cloud,
+      location: {
+        country: weatherData.location.country,
+        city: weatherData.location.name,
+        time: weatherData.location.localtime,
+      },
+    };
+    console.log(myWeatherData);
+    return myWeatherData;
+  }
+
+  
+  export {getCurrentWeatherData, processWeatherData} 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
-// add webpack, linter, prettier and so on
-
-async function getCurrentWeatherData(location) {
-  try {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=7a84c19e7f6245b7814141628240402&q=${location}`,
-    );
-    const weatherData = await response.json();
-    // const weatherDatas = weatherData.current.temp_c;
-    //  console.log(weatherDatas)
-    // console.log(weatherData)
-    return weatherData;
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-function processWeatherData(weatherData) {
-  const myWeatherData = {
-    cloud: weatherData.current.cloud,
-  };
-  console.log(myWeatherData);
-  return myWeatherData;
-}
+import { getCurrentWeatherData, processWeatherData } from "./api";
 
 async function getD() {
   const myData = await getCurrentWeatherData("kyiv");
