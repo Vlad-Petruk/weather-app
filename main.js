7fda6260799dbcf785f05ce57be081c213b697a2
Vlad-Petruk <vladpetruk00@gmail.com>
2024-02-13 16:01:09 +0200

    with working expermental fetching and processing data

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-let getCurrentWeatherData = (location) => {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=7a84c19e7f6245b7814141628240402&q=${location}`
-    )
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(response) {
-        return response;
-      })
-    .catch(e=>{console.log(e)})
+// add webpack, linter, prettier and so on
+
+async function getCurrentWeatherData (location) {
+    try{
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=7a84c19e7f6245b7814141628240402&q=${location}`
+        )
+        const weatherData = await response.json();
+        // const weatherDatas = weatherData.current.temp_c;
+        //  console.log(weatherDatas)
+        // console.log(weatherData)
+        return weatherData
+    }
+    catch {
+        e=>{console.log(e)}
+    }
+
 }
 
-function processWeatherData(data) {
-
+function processWeatherData(weatherData) {
+    const myWeatherData = {
+       cloud: weatherData.current.cloud
+    }
+    console.log(myWeatherData)
+    return myWeatherData
 }
 
-getCurrentWeatherData('kyiv')
+async function getD (){
+    const myData = await getCurrentWeatherData('kyiv');
+    console.log(myData)
+    const parsedData = processWeatherData(myData)
+    console.log(parsedData.cloud)
+}
+getD()
+
